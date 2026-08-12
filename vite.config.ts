151ba4b0115114aa389183d4306586e956fa917c
Zubair Ahmad <zubair.ahmad@morningstar.com>
2026-08-12 22:36:20 +0530

    chore(): fixed base path issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
@@ -11,5 +10,5 @@ export default defineConfig({
       '@': '/src',
     },
   },
-  base:"wedding-invitation/",
+  base:"/wedding-invitation",
 })
